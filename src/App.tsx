178bfcd01c32fa461b591325f8d1f9851f1746a8
7lad48 <2335213@gmail.com>
2023-04-26 12:39:32 +0300

    add task 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,23 @@ function App() {
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
     ]
-
+    const tasks2: TaskType[] = [
+        {id: 1, title: "HTML & CSS bookmarks", isDone: true},
+        {id: 2, title: "learn js", isDone: true},
+        {id: 3, title: "React Documentation", isDone: false},
+    ]
 
     return (
         <div className="App">
             <TodoList
                 tasks={tasks}
                 title="What to learn"/>
+            <TodoList
+                tasks={tasks2}
+                title="What to read"/>
         </div>
     );
 }
 
 export default App;
+
